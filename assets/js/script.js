411fe979a922ef1a1dc74aad454a8d503a32be4c
Ranyanna Ayala <ranyanna-@hotmail.com>
2026-05-04 16:43:41 -0400

    feat: adiciona evento de submit no formulário

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
 const form = document.querySelector('form')
 const input = document.querySelector('input')
 const results = document.querySelector('.results')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+})
