5986d3c5254ba48343b59dba37cc715ab35d1968
Ranyanna Ayala <ranyanna-@hotmail.com>
2026-05-04 17:32:56 -0400

    feat: adiciona fetch para a API Jikan

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,5 @@ const results = document.querySelector('.results')
 form.addEventListener('submit', (event) => {
     event.preventDefault()
     const busca = input.value
+    fetch(`https://api.jikan.moe/v4/anime?q=${busca}`)
 })
