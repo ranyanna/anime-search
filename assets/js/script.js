d96c8454713a751a4e33eb11194fb5116bad2fcd
Ranyanna Ayala <ranyanna-@hotmail.com>
2026-05-06 17:43:25 -0400

    feat: salva favoritos no localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,28 +8,36 @@ form.addEventListener('submit', async (event) => {
     const busca = input.value
 
     try {
-    loadingResults.classList.remove('hidden')
-    const resposta = await fetch(`https://api.jikan.moe/v4/anime?q=${busca}`)
-    const dados = await resposta.json()
-    const animes = dados.data
-    results.innerHTML = ''
-    if (animes.length === 0) {
-        results.innerHTML = `<p>Nenhum resultado encontrado</p>`
-    } else {
-    animes.forEach(anime => {
-        const card = document.createElement('div') 
-        card.classList.add('card')
-        results.appendChild(card)  
-        card.innerHTML = `<img src="${anime.images.jpg.image_url}"> <h3>${anime.title}</h3> <p>${anime.genres.map(genero => genero.name).join(', ')}</p>`
-    });
+        loadingResults.classList.remove('hidden')
+        const resposta = await fetch(`https://api.jikan.moe/v4/anime?q=${busca}`)
+        const dados = await resposta.json()
+        const animes = dados.data
+        results.innerHTML = ''
+        if (animes.length === 0) {
+            results.innerHTML = `<p>Nenhum resultado encontrado</p>`
+        } else {
+            animes.forEach(anime => {
+                const card = document.createElement('div')
+                card.classList.add('card')
+                results.appendChild(card)
+                card.innerHTML = `<img src="${anime.images.jpg.image_url}"> <h3>${anime.title}</h3> <p>${anime.genres.map(genero => genero.name).join(', ')}</p> <button onclick="favoritar('${anime.title}', '${anime.images.jpg.image_url}')">♡ Favoritar</button>`
+            });
+        }
+    } catch (erro) {
+        results.innerHTML = `<p>Servidor nao encontrado</p>`
+    } finally {
+        loadingResults.classList.add('hidden')
     }
-} catch (erro) {
-    results.innerHTML = `<p>Servidor nao encontrado</p>`
-} finally {
-    loadingResults.classList.add('hidden')
-}
 })
 
 
-    
+
+function favoritar(titulo, imagem) {
+    console.log(titulo, imagem)
+
+    const favoritosAtuais = JSON.parse(localStorage.getItem('favoritos')) || []
+    favoritosAtuais.push({ titulo, imagem })
+
+    localStorage.setItem('favoritos', JSON.stringify(favoritosAtuais))
+}
 
